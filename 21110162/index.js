8e8884546f8bade91af37c9625259af367052906
tri-dat-111003 <tridat1110312@gmail.com>
2023-10-14 01:44:50 +0700

    Update index.js

--- 21110162/index.js
+++ 21110162/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// Middleware log
+const logRequest = (req, res, next) => {
+  const method = req.method; // Lấy phương thức (GET hoặc POST)
+  const url = req.originalUrl; // Lấy URL
+
+  console.log(`Received a ${method} request to ${url}`);
+  next(); // Tiếp tục xử lý request
+};
+
+// Sử dụng middleware log trước các route
+app.use(logRequest);
+
 app.use(express.json());
 
 // Import routes
